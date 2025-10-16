8ba753663cb126b1d3235d97f5065f490cf92b10
Peer <r.m.peer@student.tue.nl>
2025-10-16 12:02:51 +0200

    removed vatican city, created fitting DB name

--- mappin/src/main/java/mappin/game/Main.java
+++ mappin/src/main/java/mappin/game/Main.java
@@ -5,14 +5,8 @@ package mappin.game;
  */
 public class Main {
     public static void main(String[] args) {
-        String dbURL = "jdbc:h2:./todo;AUTO_SERVER=TRUE";
+        String dbURL = "jdbc:h2:./mappin;AUTO_SERVER=TRUE";
         DBmanager testing = new DBmanager(dbURL);
-        System.out.println(testing.getStrQueryResult("select * from cities where name='London'"));
-        float[] coordinates = testing.getCityLocation("London");
-        System.out.println(coordinates[0] + " " + coordinates[1]);
-        int[] rgb = testing.getountryColor("United Kingdom");
-        System.out.println(rgb[0] + " " + rgb[1] + " " + rgb[2]);
-        System.out.println(testing.getCityById(88));
-        System.out.println(testing.getCountryById(88));
+        System.out.println(testing.getCityById(90));
     }
 }
