4cfc2c8d010cdf02dfc4987ccc2e778b46f4a898
Peer <r.m.peer@student.tue.nl>
2025-10-23 21:47:09 +0200

    custom label documentation

--- mapPIN/src/main/java/mappin/game/CustomLabel.java
+++ mapPIN/src/main/java/mappin/game/CustomLabel.java
@@ -1,10 +1,18 @@
 package mappin.game;
 
+import java.awt.Font;
 import javax.swing.*;
-import java.awt.*;
 
+/**
+ * A class for a custom Jlabel with non default text and fontsize.
+ */
 public class CustomLabel extends JLabel {
 
+    /**
+     * Object constructor, creates a JLabel according to given parameters.
+     * @param text String for label to showcase.
+     * @param fontSize Size of text in the label.
+     */
     CustomLabel(String text, int fontSize) {
         super(text, JLabel.CENTER);
         this.setFont(new Font(this.getFont().getName(), this.getFont().getStyle(), fontSize));
