3a640dc5d171680965269abadadc43e2830f07bc
Peer <r.m.peer@student.tue.nl>
2025-10-23 19:24:42 +0200

    finished DB documentation

--- mapPIN/src/main/java/mappin/game/DBmanager.java
+++ mapPIN/src/main/java/mappin/game/DBmanager.java
@@ -9,7 +9,16 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
- * bla bla bla.
+ * this class handles all connections and queries to the DB.
+ * it contains generic methods for executing commands and queries on the DB,
+ * as well as specific methods for the queries necessary for the game.
+ * it requires a URL to connect to, and will not work outside of a java project.
+ * for more info read the documentation of specific methods.
+ * the DB contains two tables:
+ * table cities, with the collumns of:
+ * id(integer), name(varchar), latitude(decimal), longitude(decimal), country(varchar)
+ * table country_colors with collumns of:
+ * id(integer), name(varchar), red(integer), green(integer), blue(integer).
  */
 public class DBmanager {
 
@@ -17,22 +26,27 @@ public class DBmanager {
     private Statement statement;
 
     /**
-     * bla.
-     * @param connectionUrl url of db to connect to.
+     * object constructor.
+     * When given a url to connect to it creates both a connection and statement object.
+     * these will not be accesible outside the class itself, as there should be no access
+     * to the DB outside of this class.
+     * @param connectionUrl url of DB to connect to.
      */
     DBmanager(String connectionUrl) {
         try {
             this.connection = DriverManager.getConnection(connectionUrl);
-            this.statement = connection.createStatement();   
-            System.out.println(this.connection);
+            this.statement = connection.createStatement();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     /**
-     * bla.
-     * @param dbCommandStr command to run on db.
+     * a method to execute statements on the DB.
+     * this method was used to populate the DB itself, as well as updaet or remove values.
+     * However, it has no need during the gameplay itself.
+     * in case more locations will be added, removed or chagned manually in the DB it may be used.
+     * @param dbCommandStr command to run through DB connection.
      */
     public void executeStatement(String dbCommandStr) {
         try {    
@@ -43,7 +57,9 @@ public class DBmanager {
     }
 
     /**
-     * bla.
+     * a method to execute any query given by a string.
+     * this will have no use during gameplay, as other methods contain all specific 
+     * queries used in the gameplay.
      * @param queryStr string of query to run.
      * @return result set of query result. if fails, null.
      */
@@ -57,23 +73,33 @@ public class DBmanager {
     }
 
     /**
-     * bla.
+     * return a result of a query to a DB as a string, so it could be easily printed.
+     * uses the executeQuery method to get a result set. 
+     * once it is received, the method iterates line by line on the result set 
+     * (moving line to line by using resultset.next()), and gathers all objects in each entry.
+     * all objects gatheres are then turned to strings and consutructed into a line of text,
+     * which will be a part of the final resultString.
      * @param queryStr query to execute with DB.
      * @return string of result from db
      */
     public String getStrQueryResult(String queryStr) {
         String resulString;
+        // obtain query result
         ResultSet resultSet = executeQuery(queryStr);
         try {
             resulString = "";
+            //obtain number of collumns (which is also the num of objects in each line)
             ResultSetMetaData meta = resultSet.getMetaData();
             int collumns = meta.getColumnCount();
+            // iterate over the resultset entry by entry.
             while (resultSet.next()) { 
                 String line = "";
+                // add all data in an entry to a line of text
                 for (int i = 1; i < collumns; i++) {
                     line += resultSet.getObject(i).toString() + " ";
                 }
                 line += resultSet.getObject(collumns).toString();
+                // based if there are any more entries, add newline character or not.
                 if (resultSet.isLast()) {
                     resulString += line;
                 } else {
@@ -88,7 +114,8 @@ public class DBmanager {
     }
 
     /**
-     * bla.
+     * return the coordinates of a city, using its name,
+     * by executing a query on the DB.
      * @param city name of city to get coordinates for.
      * @return array of floats representing coordinates.
      */
@@ -107,7 +134,9 @@ public class DBmanager {
     }
 
     /**
-     * bla.
+     * return the color of a country in the beckend map, using its name.
+     * the color is represented via RGB integer values.
+     * do this via a query to the DB.
      * @param country country to find.
      * @return int array of rgb color breakdown.
      */
@@ -128,9 +157,10 @@ public class DBmanager {
     }
 
     /**
-     * bla.
-     * @param id id chosen randomly.
-     * @return name of city.
+     * given a number, return the relevant city name in the DB.
+     * each city is designated a unique numeric id in the DB.
+     * @param id number of city in the DB.
+     * @return string name ofcity with given id.
      */
     public String getCityById(Integer id) {
         String query = "select name from cities where id='%s'".formatted(id);
@@ -145,9 +175,10 @@ public class DBmanager {
     }
 
     /**
-     * bla.
-     * @param id bla.
-     * @return bla.
+     * given a number, return the relevant country name in the DB.
+     * each country is designated a unique numeric id in the DB.
+     * @param id number of country in the DB.
+     * @return string name of country with given id.
      */
     public String getCountryById(Integer id) {
         String query = "select name from country_colors where id='%s'".formatted(id);
